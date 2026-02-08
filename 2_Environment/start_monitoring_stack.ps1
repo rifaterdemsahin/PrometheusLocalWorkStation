30ceecc6d0a41694d02a3256f9a97f262a7ee65a
Rifat Erdem Sahin <info@pexabo.com>
2026-02-08 14:09:37 +0000

    Revert: Remove scheduled task script due to missing Docker dependency

--- 2_Environment/start_monitoring_stack.ps1
+++
@@ -1,5 +0,0 @@
-# Navigate to the directory containing docker-compose.yml
-Set-Location -Path "C:\projects\PrometheusLocalWorkStation\2_Environment"
-
-# Start Docker Compose services in detached mode
-docker-compose -f docker-compose.yml up -d
